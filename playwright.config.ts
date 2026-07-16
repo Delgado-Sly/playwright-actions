53ad6cc2d6bb6372e2d5e832c554aff6b7c483eb
Salomão Delgado <delgadosly2006@gmail.com>
2026-07-16 15:59:29 +0100

    simulando a correção do bug e testes multi-browser

--- playwright.config.ts
+++ playwright.config.ts
@@ -41,7 +41,6 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    /*
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
@@ -51,7 +50,6 @@ export default defineConfig({
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
-    */
 
     /* Test against mobile viewports. */
     // {
